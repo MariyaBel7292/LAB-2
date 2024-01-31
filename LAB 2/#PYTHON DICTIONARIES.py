392ce0cb1bd97d87236e0c0968645b357d85c6c1
MariyaBel7292 <156871668+MariyaBel7292@users.noreply.github.com>
2024-01-31 23:48:48 +0600

    Add files via upload

---
+++ LAB 2/#PYTHON DICTIONARIES.py
@@ -0,0 +1,36 @@
+#PYTHON DICTIONARIES
+#ex1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#ex2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+
+#ex3
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+
+#ex4
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+#ex5
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
